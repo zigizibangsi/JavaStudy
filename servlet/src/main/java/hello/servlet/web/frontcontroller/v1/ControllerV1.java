c828b4580ec5e4e5b86c44e9d7c235640af11382
zigizibangsi <zigizibangsi@gmail.com>
2024-05-11 22:53:40 +0900

    1st

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -0,0 +1,10 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ControllerV1 {
+
+    void process(HttpServletRequest request, HttpServletResponse response) throws ServletException,  
+}
